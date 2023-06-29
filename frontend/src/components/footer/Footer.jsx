aad14066148ebb8fbbd02330c6725fc3d5c7ada6
Roxanne Lucas <lucas.roxanne@gmail.com>
2023-06-29 16:52:14 +0200

    login done

---
+++ frontend/src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './footer.css';
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <span>Mentions légales</span>
+      <span>Politique de confidentialité</span>
+      <span>© Emmaüs Connect avec LaCollecte.tech</span>
+    </div>
+  );
+};
+
+export default Footer;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Footer from '../components/footer/Footer';
 import './home.css';
+import Login from '../components/login/Login';
 
 const Home = () => {
+  const navigate = useNavigate();
+  const [loginPage, setLoginPage] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoginPage(true);
+  };
+
+  const handleClick = () => {
+    setLoginPage(false);
+  };
+
   return (
-    <div>
-      <h1>L'accès à la technologie pour tous, un smartphone à la fois.</h1>
+    <div className='home'>
+      {loginPage && <Login handleClick={handleClick} />}
+      <div className='text-presentation'>
+        <h1>
+          L'accès à la technologie pour tous,{' '}
+          <span style={{ color: 'var(--second-color)' }}>un smartphone</span> à
+          la fois.
+        </h1>
+        <p>
+          Ensemble, nous sommes déterminés à faire une différence dans la vie
+          des personnes exclues, en leur offrant les opportunités et les outils
+          nécessaires pour s'épanouir dans le monde numérique.
+        </p>
+        <button className='login-btn' onClick={handleSubmit}>
+          Se connecter
+        </button>
+      </div>
+      <Footer />
     </div>
   );
 };
